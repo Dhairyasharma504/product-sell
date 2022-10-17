7aaba17969f2ec82946fb4036f7937acc853916a
Dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-10-18 01:28:03 +0530

    done

--- src/pages/dashboard/profile.js
+++ src/pages/dashboard/profile.js
@@ -6,12 +6,20 @@ const Profile = () => (
   <div className="">
     <LayoutDashboard>
       <div className="p-3">
-        <div className="flex space-x-6">
-          <Input text="first-name" type="text" placeholder="first-name" />{' '}
-          <Input text="last-name" type="text" placeholder="last-name" />{' '}
+        <div className="mb-4 ">
+          <img
+            src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+            className=" h-40 w-40 rounded-full hover:opacity-50"
+            alt="photo"
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-7  items-center">
+          <Input text="first-name" type="text" placeholder="first-name" />
+          <Input text="last-name" type="text" placeholder="last-name" />
         </div>
         <Input text="contact Number" type="text" placeholder="contact Number" />
         <Input text="email" type="text" placeholder="email" />
+        <Input text="business name" type="text" placeholder="business name" />
         <Input text="address-1" type="text" placeholder="address-1" />
         <Input text="address-2" type="text" placeholder="address-2" />
       </div>
