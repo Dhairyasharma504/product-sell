2befbca223c559810ec2aa07cfc4aefc24e506c9
dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-10-28 13:32:05 +0530

    order page delete button working

--- src/pages/dashboard/orders.js
+++ src/pages/dashboard/orders.js
@@ -1,9 +1,30 @@
 // import dynamic from 'next/dynamic';
 
+import { IoMdTrash } from 'react-icons/io';
+import Swal from 'sweetalert2';
 import LayoutDashboard from '../../components/DashboardLayout';
 import TableHead from '../../components/elements/TableHead';
 
-export default function Orders() {
+const Orders = () => {
+  const handleRemove = () => {
+    Swal.fire({
+      title: 'Do you want to delete the product?',
+      icon: 'question',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      denyButtonText: `No`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Success!',
+          'Your product is deleted successfully.',
+          'success',
+        );
+      }
+    });
+  };
   return (
     <LayoutDashboard>
       <div className="mt-8">
@@ -86,22 +107,24 @@ export default function Orders() {
                           d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                         />
                       </svg>
-                    </div>
-                    <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                        />
-                      </svg>
-                    </div>
+                    </div>{' '}
+                    <button onClick={handleRemove} type="button" className="">
+                      <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                          />
+                        </svg>
+                      </div>
+                    </button>
                   </div>
                 </td>
               </tr>
@@ -111,4 +134,5 @@ export default function Orders() {
       </div>
     </LayoutDashboard>
   );
-}
+};
+export default Orders;
