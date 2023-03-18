357f93f9a1354dbc4bfe7996c1c262da75c7091d
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-03-18 13:28:47 +0530

    add feilds

--- schemas/items.js
+++ schemas/items.js
@@ -3,6 +3,12 @@ export default {
   name: 'product',
   type: 'document',
 	title: 'Products',
+
+
+
+
+
+
   initialValue: {
     status: "active"
   },
@@ -33,11 +39,60 @@ export default {
           type: "object",
           name: "variant",
           fields: [
-            { type: "string", name: "name", title:"Name" },
-            { type: "number", name: "price", title:"Price" }
+            { type: "string", name: "type", title:"Type",
+            initialValue: 'type',
+            options: {
+              list: [
+                { title: 'PPR', value: 'ppr' },
+                { title: 'CPVC', value: 'cpvc' },
+                { title: 'PVC',  value: 'pvc' },
+
+              ],
+            }
+          },
+            { type: "string", name: "companyname", title:"Company Name"  ,
+            initialValue: 'type',
+            options: {
+              list: [
+                { title: 'Prince', value: 'prince' },
+                { title: 'Finolex', value: 'finolex' },
+                { title: 'Euro',  value: 'euro' }, { title: 'Fusion',  value: 'fusion' }, { title: 'Apollo',  value: 'Apollo' },
+
+              ],
+            }
+
+          },
+            { type: "number", name: "price", title:"Price" },
+            { type: "string", name: "size", title:"Size",
+            initialValue: 'size',
+            options: {
+              list: [
+                { title: '15mm', value: '15mm' },
+                { title: '20mm', value: '20mm' },
+                { title: '25mm',  value: '25mm' },
+                { title: '32mm',  value: '32mm' },
+                { title: '40mm',  value: '40mm' },
+                { title: '50mm',  value: '50mm' },
+                { title: '20mm x 15mm', value: '20mm x 15mm' },
+                { title: '25mm x 15mm', value: '25mm x 15mm' },
+                { title: '25mm x 20mm',  value: '25mm x 20mm' },
+                { title: '40mm x 25mm',  value: '40mm x 25mm' },
+                { title: '50mm x 25mm',  value: '50mm x 25mm' },
+                { title: '50mm x 40mm',   value: '50mm x 40mm' },
+              ], // <-- predefined values
+              //layout: 'radio' // <-- defaults to 'dropdown'
+            }
+
+          }
           ]
         }
       ],
+
+
     }
+
   ]
+
+
+
 }
