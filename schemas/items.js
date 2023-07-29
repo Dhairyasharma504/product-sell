a5223c5fd98ca7553e07f0b37521a265f26eba80
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-07-29 10:40:57 +0530

    add items

--- schemas/items.js
+++ schemas/items.js
@@ -2,21 +2,15 @@
 export default {
   name: 'product',
   type: 'document',
-	title: 'Products',
-
-
-
-
-
-
+  title: 'Products',
   initialValue: {
-    status: "active"
+    status: 'active',
   },
   fields: [
     {
       name: 'name',
       type: 'string',
-      title: 'Name'
+      title: 'Name',
     },
     {
       title: 'Status',
@@ -25,74 +19,118 @@ export default {
       options: {
         list: [
           {title: 'Active', value: 'active'},
-          {title: 'Not Active', value: 'notActive'}
+          {title: 'Not Active', value: 'notActive'},
         ], // <-- predefined values
-      }
+      },
     },
     {
-      title: "Variants",
-      type: "array",
-      name: "variants",
-      validation: Rule => Rule.min(1),
+      type: 'string',
+      name: 'companyname',
+      title: 'Brand',
+      initialValue: 'type',
+      options: {
+        list: [
+          {title: 'Prince', value: 'prince'},
+          {title: 'Prince type A', value: 'princetype'},
+          {title: 'Finolex', value: 'finolex'},
+          {title: 'Finolex A', value: 'finolextype'},
+          {title: 'Euro', value: 'euro'},
+          {title: 'D.T Plastic KG', value: 'd.tplastic'},
+          {title: 'Fusion', value: 'fusion'},
+          {title: 'Apollo', value: 'Apollo'},
+          {title: 'Ravindra', value: 'ravindra'},
+          {title: 'Jindal', value: 'jindal'},
+          {title: 'Kalsi', value: 'kalsi'},
+          {title: 'K.V.S', value: 'k.v.s'},
+          {title: 'Cromptan', value: 'cromptan'},
+          {title: 'Taj', value: 'taj'},
+          {title: 'Not Registered', value: 'notRegistered'},
+        ],
+      },
+    },
+    {
+      title: 'Variants',
+      type: 'array',
+      name: 'variants',
+      validation: (Rule) => Rule.min(1),
       of: [
         {
-          type: "object",
-          name: "variant",
+          type: 'object',
+          name: 'variant',
           fields: [
-            { type: "string", name: "type", title:"Type",
-            initialValue: 'type',
-            options: {
-              list: [
-                { title: 'PPR', value: 'ppr' },
-                { title: 'CPVC', value: 'cpvc' },
-                { title: 'PVC',  value: 'pvc' },
+            {type: 'string', name: 'title', title: 'Title-Size-Quantity'},
+            {
+              type: 'string',
+              name: 'type',
+              title: 'Type',
+              initialValue: '',
+              options: {
+                list: [
+                  {title: 'PPR', value: 'ppr'},
+                  {title: 'CPVC', value: 'cpvc'},
+                  {title: 'PVC', value: 'pvc'},
+                  {title: 'GI', value: 'gi'},
+                  {title: 'MS', value: 'ms'},
+                  {title: 'UPVC', value: 'upvc'},
+                  {title: 'CI', value: 'ci'},
+                  {title: 'Brass', value: 'brass'},
+                  {title: 'Steel', value: 'steel'},
+                  {title: 'Moter', value: 'moter'},
+                  {title: 'Mini Hand Pump', value: 'miniHandpump'},
+                  {title: 'Water Tank', value: 'waterTank'},
+                  {title: 'Plasti Tap', value: 'plastitap'},
+                  {title: 'CP Fitting', value: 'cpfitting'},
+                  {title: 'Plastic Fitting', value: 'plasticfitting'},
+                  {title: 'seat', value: 'seat'},
+                ],
+              },
+            },
 
+            {type: 'number', name: 'price', title: 'Price'},
+
+            {
+              title: 'Images',
+              name: 'images',
+              type: 'image',
+              of: [
+                {
+                  type: 'image',
+                  options: {
+                    hotspot: true,
+                  },
+                },
               ],
-            }
-          },
-            { type: "string", name: "companyname", title:"Company Name"  ,
-            initialValue: 'type',
-            options: {
-              list: [
-                { title: 'Prince', value: 'prince' },
-                { title: 'Finolex', value: 'finolex' },
-                { title: 'Euro',  value: 'euro' }, { title: 'Fusion',  value: 'fusion' }, { title: 'Apollo',  value: 'Apollo' },
+            },
+            //   { type: "string", name: "size", title:"Size",
+            //   initialValue: 'size',
+            //   options: {
+            //     list: [
+            //       { title: '15mm', value: '15mm' },
+            //       { title: '20mm', value: '20mm' },
+            //       { title: '25mm',  value: '25mm' },
+            //       { title: '32mm',  value: '32mm' },
+            //       { title: '40mm',  value: '40mm' },
+            //       { title: '50mm',  value: '50mm' },
+            //       { title: '20mm x 15mm', value: '20mm x 15mm' },
+            //       { title: '25mm x 15mm', value: '25mm x 15mm' },
+            //       { title: '25mm x 20mm',  value: '25mm x 20mm' },
+            //       { title: '40mm x 25mm',  value: '40mm x 25mm' },
+            //       { title: '50mm x 25mm',  value: '50mm x 25mm' },
+            //       { title: '50mm x 40mm',   value: '50mm x 40mm' },
+            //     ], // <-- predefined values
+            //     //layout: 'radio' // <-- defaults to 'dropdown'
+            //   }
 
-              ],
-            }
-
-          },
-            { type: "number", name: "price", title:"Price" },
-            { type: "string", name: "size", title:"Size",
-            initialValue: 'size',
-            options: {
-              list: [
-                { title: '15mm', value: '15mm' },
-                { title: '20mm', value: '20mm' },
-                { title: '25mm',  value: '25mm' },
-                { title: '32mm',  value: '32mm' },
-                { title: '40mm',  value: '40mm' },
-                { title: '50mm',  value: '50mm' },
-                { title: '20mm x 15mm', value: '20mm x 15mm' },
-                { title: '25mm x 15mm', value: '25mm x 15mm' },
-                { title: '25mm x 20mm',  value: '25mm x 20mm' },
-                { title: '40mm x 25mm',  value: '40mm x 25mm' },
-                { title: '50mm x 25mm',  value: '50mm x 25mm' },
-                { title: '50mm x 40mm',   value: '50mm x 40mm' },
-              ], // <-- predefined values
-              //layout: 'radio' // <-- defaults to 'dropdown'
-            }
-
-          }
-          ]
-        }
+            // }
+          ],
+        },
       ],
-
-
-    }
-
-  ]
-
-
-
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'companyname',
+    },
+  },
 }
